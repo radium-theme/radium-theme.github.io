fa028ca4ee3cf3e69a76930433891f9945a0e79b
PineappleRind <pineapplerind.info@gmail.com>
2021-10-09 10:43:49 -0400

    Site

---
+++ pageLoad.js
@@ -0,0 +1,17 @@
+if ('scrollRestoration' in history) {
+    history.scrollRestoration = 'manual';
+}
+window.scrollTo(0, 0);
+
+let arr = [
+    'title',
+    'subTitle',
+    'toLoad',
+    'github',
+    'vscode',
+]
+for (let i = 0; i < arr.length; i++) {
+    setTimeout(function () {
+        document.getElementById(arr[i]).classList.remove('notLoaded')
+    }, i * 10)
+}
